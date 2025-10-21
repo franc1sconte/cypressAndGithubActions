abb033abbe798043bf1aab382badaea7d9d1ad21
franc1sconte <francisconte11@hotmail.com>
2025-10-21 17:29:48 -0300

    algo q se me olvido guardar

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -2,66 +2,66 @@
 
 describe('example', () => {
   beforeEach(() => {
-    
+
   })
 
   it('prueba login 1', () => {
-    
-  const authState = {
-    state: {
-      user: {
-        id: Cypress.env('USER_ID_1'),
-        name: Cypress.env('USER_NAME_1'),
-        email: Cypress.env('USER_EMAIL_1'),
-        avatar: Cypress.env('USER_AVATAR_1'),
-        wasOnboarded: Cypress.env('WAS_ON_BOARDED')
+
+    const authState = {
+      state: {
+        user: {
+          id: Cypress.env('USER_ID_1'),
+          name: Cypress.env('USER_NAME_1'),
+          email: Cypress.env('USER_EMAIL_1'),
+          avatar: Cypress.env('USER_AVATAR_1'),
+          wasOnboarded: Cypress.env('WAS_ON_BOARDED')
+        },
+        token: Cypress.env('USER_TOKEN_1'),
+        isAuthenticated: Cypress.env('IS_AUTHENTICATED')
       },
-      token: Cypress.env('USER_TOKEN_1'),
-      isAuthenticated: Cypress.env('IS_AUTHENTICATED')
-    },
-    version: 0
-  };
+      version: 0
+    };
 
-  // Inyectar el auth en localStorage antes de que la app cargue
-  cy.visit('/', {
-    onBeforeLoad(win) {
-    win.localStorage.setItem('auth', JSON.stringify(authState));
-    }
-  })
+    // Inyectar el auth en localStorage antes de que la app cargue
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        win.localStorage.setItem('auth', JSON.stringify(authState));
+      }
+    })
 
-  cy.log('Auth hardcodeado inyectado:', authState);
-  cy.url().should('include', '/dashboard')
-  // cy.screenshot('dashboard-authenticated')
+    cy.log('Auth hardcodeado inyectado:', authState);
+    cy.url().should('include', '/dashboard')
+    // cy.screenshot('dashboard-authenticated')
 
   })
 
-it('prueba login 2', () => {
-    
-  const authState = {
-    state: {
-      user: {
-        id: Cypress.env('USER_ID_2'),
-        name: Cypress.env('USER_NAME_2'),
-        email: Cypress.env('USER_EMAIL_2'),
-        avatar: Cypress.env('USER_AVATAR_2'),
-        wasOnboarded: Cypress.env('WAS_ON_BOARDED')
+  it('prueba login 2', () => {
+
+    const authState = {
+      state: {
+        user: {
+          id: Cypress.env('USER_ID_2'),
+          name: Cypress.env('USER_NAME_2'),
+          email: Cypress.env('USER_EMAIL_2'),
+          avatar: Cypress.env('USER_AVATAR_2'),
+          wasOnboarded: Cypress.env('WAS_ON_BOARDED')
+        },
+        token: Cypress.env('USER_TOKEN_2'),
+        isAuthenticated: Cypress.env('IS_AUTHENTICATED')
       },
-      token: Cypress.env('USER_TOKEN_2'),
-      isAuthenticated: Cypress.env('IS_AUTHENTICATED')
-    },
-    version: 0
-  };
+      version: 0
+    };
 
-  // Inyectar el auth en localStorage antes de que la app cargue
-  cy.visit('/', {
-    onBeforeLoad(win) {
-    win.localStorage.setItem('auth', JSON.stringify(authState));
-    }
-  })
+    // Inyectar el auth en localStorage antes de que la app cargue
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        win.localStorage.setItem('auth', JSON.stringify(authState));
+      }
+    })
 
-  cy.log('Auth hardcodeado inyectado:', authState);
-  cy.url().should('include', '/dashboard')
-  // cy.screenshot('dashboard-authenticated')
+    cy.log('Auth hardcodeado inyectado:', authState);
+    cy.url().should('include', '/dashboard')
+    // cy.screenshot('dashboard-authenticated')
 
   })
 
