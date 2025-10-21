94834f40716ca6ea7c2fd7bbda09d55838a55666
franc1sconte <francisconte11@hotmail.com>
2025-10-21 17:23:20 -0300

    github actions

--- cypress.config.js
+++ cypress.config.js
@@ -4,35 +4,24 @@ const path = require('path');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: process.env.BASE_URL,
+    env: {
+      USER_ID_2: process.env.USER_ID_2,
+      USER_NAME_2: process.env.USER_NAME_2,
+      USER_EMAIL_2: process.env.USER_EMAIL_2,
+      USER_AVATAR_2: process.env.USER_AVATAR_2,
+      USER_TOKEN_2: process.env.USER_TOKEN_2,
+      USER_ID_1: process.env.USER_ID_1,
+      USER_NAME_1: process.env.USER_NAME_1,
+      USER_EMAIL_1: process.env.USER_EMAIL_1,
+      USER_AVATAR_1: process.env.USER_AVATAR_1,
+      USER_TOKEN_1: process.env.USER_TOKEN_1,
+      WAS_ON_BOARDED: process.env.WAS_ON_BOARDED,
+      IS_AUTHENTICATED: process.env.IS_AUTHENTICATED
+    },
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-
-      // Determine the environment to load: use CYPRESS_ENV or default to 'staging'
-      const envName = (process.env.CYPRESS_ENV || 'staging').toLowerCase();
-      const envFile = path.join(__dirname, 'env', `${envName}.json`);
-
-      if (fs.existsSync(envFile)) {
-        try {
-          const envData = JSON.parse(fs.readFileSync(envFile, 'utf8'));
-          // Merge file variables into config.env
-          config.env = Object.assign({}, config.env || {}, envData);
-
-          // If baseUrl is present in env file, set Cypress baseUrl
-          if (envData.baseUrl) {
-            config.baseUrl = envData.baseUrl;
-          }
-        } catch (err) {
-          // if parsing fails, log and continue with default config
-          // eslint-disable-next-line no-console
-          console.error(`Failed to parse environment file ${envFile}:`, err);
-        }
-      } else {
-        // eslint-disable-next-line no-console
-        console.warn(`Environment file not found: ${envFile}. Using default config.`);
-      }
-
-      return config;
     },
   },
   embeddedScreenshots: true,
